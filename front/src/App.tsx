268b087b0ea123ccefa88f54ca80a74946d15884
WooT <lindorya@hotmail.fr>
2023-03-01 01:23:57 +0100

    Ajout d'une fonction qui collore les cases en fonction du tag + split du code en plusieurs fichiers

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,70 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import './App.css';
+import RenderGrid from './components/RenderGrid';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 
-function App() {
-  const [planets, setPlanets] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:3030/planets')
-      .then(response => setPlanets(response.data))
-      .catch(error => console.log(error));
-  }, []);
-
-  // Fonction pour générer la grille orthonormée
-  function renderGrid() {
-    const grid = [];
-    const maxCoord = 31; // maximum des coordonnées x et y
-
-    // Générer chaque ligne de la grille
-    for (let y = maxCoord; y >= -maxCoord; y--) {
-      const row = [];
-
-      // Générer chaque cellule de la ligne
-      for (let x = -maxCoord; x <= maxCoord; x++) {
-        // Vérifier si la cellule contient des planètes
-        if (planets[x] && planets[x][y]) {
-          const cell = (
-            <td key={`${x},${y}`} className="cell">
-              <tr className="cell-coord">({x}, {y})</tr>
-              {planets[x][y].map(planet => (
-                <div key={planet.name} id={planet.name}>
-                  <p>{planet.name}</p>
-                  {/* Ajoutez ici les autres informations de la planète */}
-                </div>
-              ))}
-            </td>
-          );
-          row.push(cell);
-        } else {
-          row.push(<td key={`${x},${y}`}></td>);
-        }
-      }
-
-      // Ajouter la ligne à la grille
-      grid.push(<tr key={y}>{row}</tr>);
-    }
-
-    return grid;
-  }
-
+const App: React.FC = () => {
   return (
-      <table className="grid">
-        <TransformWrapper
-          centerOnInit={true}
-          initialScale={0.5}
-          limitToBounds={false}
-          minScale={0.05}
-          maxScale={3}
-        >
-          <TransformComponent>
-            <tbody className="tbody">
-                {renderGrid()}
-            </tbody>
-          </TransformComponent>
-        </TransformWrapper>
-      </table>
+    <div>
+      <div>
+        <p>Blabla</p>
+      </div>
+      <div className="test">
+        <table className="grid">
+          <TransformWrapper
+            centerOnInit={true}
+            initialScale={0.5}
+            limitToBounds={false}
+            minScale={0.05}
+            maxScale={3}
+          >
+            <TransformComponent>
+              <tbody className="tbody">
+                <RenderGrid />
+              </tbody>
+            </TransformComponent>
+          </TransformWrapper>
+        </table>
+      </div>
+    </div>
   );
 }
 
---
+++ front/src/components/RenderGrid.tsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const RenderGrid: React.FC = () => {
+  const [planets, setPlanets] = useState([]);
+  const [isTagEnabled, setIsTagEnabled] = useState(true);
+
+  useEffect(() => {
+    axios.get('http://localhost:3030/planets')
+      .then(response => setPlanets(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
+  const getCellStyle = (planet) => {
+    let style = { backgroundColor: 'white' };
+    if (isTagEnabled && planet.tag === '[PASTA]') {
+      style.backgroundColor = 'lightblue';
+    } else if (isTagEnabled && planet.tag === '[TAG]') {
+      style.backgroundColor = 'lightgreen';
+    } else if (isTagEnabled && planet.tag === '[MNKY]') {
+      style.backgroundColor = '#EA6A95';
+    }
+    return style;
+  };
+
+  const toggleTag = () => {
+    setIsTagEnabled(!isTagEnabled);
+  }
+
+  // Fonction pour générer la grille orthonormée
+  const grid = [];
+  const maxCoord = 31; // maximum des coordonnées x et y
+
+  // Générer chaque ligne de la grille
+  for (let y = maxCoord; y >= -maxCoord; y--) {
+    const row = [];
+
+    // Générer chaque cellule de la ligne
+    for (let x = -maxCoord; x <= maxCoord; x++) {
+    // Vérifier si la cellule contient des planètes
+    if (planets[x] && planets[x][y]) {
+      const cell = (
+        <td key={`${x},${y}`}>
+          <tr>({x}, {y})</tr>
+          {planets[x][y].map(planet => (
+            <div key={planet.name} style={getCellStyle(planet)}>
+              <p>{planet.name} {planet.tag}</p>
+              {/* Ajoutez ici les autres informations de la planète */}
+            </div>
+          ))}
+        </td>
+      );
+      row.push(cell);
+    } else {
+      row.push(<td key={`${x},${y}`}></td>);
+    }}
+
+    // Ajouter la ligne à la grille
+    grid.push(<tr key={y}>{row}</tr>);
+  }
+
+  return (
+    <div>
+      <button onClick={toggleTag} style={{width: 200, height: 200}}>Toggle Tag</button>
+      <table style={{borderCollapse: 'collapse'}}>
+        <tbody>
+          {grid}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default RenderGrid;
